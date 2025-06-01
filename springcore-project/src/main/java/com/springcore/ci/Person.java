cc3ec539ae550d114bb6d3c6aa195d8ca90a175a
abhishek975 <dixitabhi1605@gmail.com>
2025-06-01 23:13:04 +0530

    Dependency Injection through Constructor for Reference Type

--- springcore-project/src/main/java/com/springcore/ci/Person.java
+++ springcore-project/src/main/java/com/springcore/ci/Person.java
@@ -3,14 +3,16 @@ package com.springcore.ci;
 public class Person {
   private String name;
   private int personId;
-public Person(String name, int personId) {
+  private Certi certi;
+public Person(String name, int personId,Certi certi) {
 	super();
 	this.name = name;
 	this.personId = personId;
+	this.certi=certi;
 }
 @Override
 public String toString() {
-	return "Person [name=" + name + ", personId=" + personId + "]";
+	return "Person [name=" + name + ", personId=" + personId + ", certificate="+certi+"]";
 }
   
 }
