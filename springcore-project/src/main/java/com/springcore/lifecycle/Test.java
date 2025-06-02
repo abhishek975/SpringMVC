fce973e62f74cdd09365f31067b977b45442f622
abhishek975 <dixitabhi1605@gmail.com>
2025-06-02 22:21:19 +0530

    SpringBeanLifeCycle through XML config

--- springcore-project/src/main/java/com/springcore/lifecycle/Test.java
+++ springcore-project/src/main/java/com/springcore/lifecycle/Test.java
@@ -2,14 +2,15 @@ package com.springcore.lifecycle;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.support.AbstractApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 ApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
+		 AbstractApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
 		 Book b=(Book) context.getBean("book");
 		 System.out.println(b.toString());
+		 context.registerShutdownHook(); //To see destroy method to run
 	}
 
 }
