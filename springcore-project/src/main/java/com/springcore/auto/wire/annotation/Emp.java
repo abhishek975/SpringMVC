aea7be40d3f610c5e9aada3d4cd0f5e396129caf
abhishek975 <dixitabhi1605@gmail.com>
2025-06-05 23:19:35 +0530

    Autowiring byType

---
+++ springcore-project/src/main/java/com/springcore/auto/wire/annotation/Emp.java
@@ -0,0 +1,32 @@
+package com.springcore.auto.wire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Emp {
+	@Autowired   //Autowire byType
+ private Address address;
+
+public Address getAddress() {
+	return address;
+}
+
+public void setAddress(Address address) {
+	this.address = address;
+}
+
+@Override
+public String toString() {
+	return "Emp [address=" + address + "]";
+}
+
+public Emp(Address address) { //Autowiring by constructor checks byType
+	super();
+	this.address = address;
+	System.out.println("Constructor is called");
+}
+
+public Emp() {  
+	super();
+	// TODO Auto-generated constructor stub
+}
+}
